5d68de6db7b39bd5908cbab4325b83f7739b6ce3
dkkulkarni287 <deepak.kulkarni287@gmail.com>
2024-06-13 12:25:15 +0530

    testing git

---
+++ Javascriptbasics.js
@@ -0,0 +1,41 @@
+// concatenation
+
+/* var fname = "Deepak"
+var lname = "Kulkarni"
+console.log(fname,lname)
+console.log (fname+" "+lname) */
+
+/*
+var i = 10
+console.log(i--)  // print 10, value 9
+console.log(--i)  // value 8, print 8
+console.log(i--)  // print 8, value 7
+console.log(i) // print 7 */
+
+// functions
+/*
+function fomula(a,b){
+
+var c = (a**2+b**2+2*a*b); 
+return c;
+
+}
+
+console.log(fomula(2,3)); */
+
+// Arrays
+
+var family = new Array ("Deepak","Shamili","Shresta");
+/*
+for (var i=0; i< family.length; i++) {
+
+console.log(family[i]);
+
+} */
+
+for (var f in family) {
+    console.log(family[f]);
+}
+
+
+
